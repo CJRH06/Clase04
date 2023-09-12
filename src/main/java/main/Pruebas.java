2e7614b197fd5a2b5afce5eb5744e0581217e1f0
El profesor <SUITE@SUITESJ202-ST06.cibertec.edu.pe>
2023-09-11 22:15:41 -0500

    Se agregó el apellido del usuario

--- src/main/java/main/Pruebas.java
+++ src/main/java/main/Pruebas.java
@@ -6,6 +6,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import javax.persistence.Query;
+
 import model.TbUsuario;
 
 public class Pruebas {
@@ -15,12 +17,25 @@ public class Pruebas {
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("JPA_clase01x");
 		EntityManager em = fabrica.createEntityManager();
 		
-		List<TbUsuario> lstUsuarios = em.createQuery("Select a From TbUsuario a", TbUsuario.class).getResultList();
+		Query query = em.createNativeQuery("{call usp_validaAcceso(?,?)}", TbUsuario.class);
+		query.setParameter(1, "U001@gmail.com");
+		query.setParameter(2, "10001");
+		
+		TbUsuario u = (TbUsuario)query.getSingleResult();
+		if (u==null) {
+			System.out.println("El usuario no existe");
+		} else {
+			System.out.println("Bienvenido: " + u.getNomUsua()+" "+u.getApeUsua());
+		}
+		
+		
+		//List<TbUsuario> lstUsuarios = query.getResultList();
 		
 		/*Mostrar cantidad de usuarios*/
-		System.out.println("nro de usuarios:" +lstUsuarios.size());
-		System.out.println("==============================================");
+		//System.out.println("nro de usuarios:" +lstUsuarios.size());
+		//System.out.println("==============================================");
 
+		
 	}
 
 }
